a5c1c2810c49d6dd127c92046083f729e619a501
JuicL <kolomin111@gmail.com>
2024-03-21 22:38:00 +0400

    Add set miss assign delivery status command

---
+++ FoodDelivery.Delivering.API/Application/Commands/CourierCommands/SetMissAssignDeliveryStatusCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace FoodDelivery.Delivering.API.Application.Commands.CourierCommands
+{
+    public record SetMissAssignDeliveryStatusCommand(long DeliveryId,long CourierId) : IRequest<bool>;
+}
---
+++ FoodDelivery.Delivering.API/Application/Commands/CourierCommands/SetMissAssignDeliveryStatusCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using FoodDelivery.Delivering.Domain.AgregationModels.AssignDeliveryAgregate;
+using MediatR;
+
+namespace FoodDelivery.Delivering.API.Application.Commands.CourierCommands
+{
+    public class SetMissAssignDeliveryStatusCommandHandler : IRequestHandler<SetMissAssignDeliveryStatusCommand, bool>
+    {
+        private readonly IAssignDeliveryRepository _assignDeliveryRepository;
+
+        public SetMissAssignDeliveryStatusCommandHandler(IAssignDeliveryRepository assignDeliveryRepository)
+        {
+            _assignDeliveryRepository = assignDeliveryRepository;
+        }
+
+        public async Task<bool> Handle(SetMissAssignDeliveryStatusCommand request, CancellationToken cancellationToken)
+        {
+            var assignDelivery = await _assignDeliveryRepository.GetByCourierAndDeliveryIdsAsync(request.DeliveryId, request.CourierId);
+            if (assignDelivery is null)
+                throw new Exception("Assigned delivery not found");
+            assignDelivery.SetMissStatus();
+            await _assignDeliveryRepository.UpdateAsync(assignDelivery);
+            return await _assignDeliveryRepository.UnitOfWork.SaveEntitiesAsync();
+        }
+    }
+}
